1b8ba36e069aef61c8fb81e996465d0bb4d5a94d
Janusz Soft <wojt03@o2.pl>
2017-10-05 18:33:37 +0200

    Initial commit

---
+++ src/main/java/com/example/drawingboard/DrawingBoardApplication.java
@@ -0,0 +1,12 @@
+package com.example.drawingboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawingBoardApplication.class, args);
+	}
+}
