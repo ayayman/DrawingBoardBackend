bee8e78e3906f2ea1dd33f2da626ccc2e6c1c334
Janusz Soft <wojt03@o2.pl>
2017-10-06 02:49:52 +0200

    Add model of board message

---
+++ src/main/java/com/example/drawingboard/BoardLineMessage.java
@@ -0,0 +1,70 @@
+package com.example.drawingboard;
+
+public class BoardLineMessage {
+
+    public enum Type { ADD, CLEAR, INFO }
+
+    private Type type;
+    private Double x0;
+    private Double y0;
+    private Double x1;
+    private Double y1;
+    private String color;
+    private Integer size;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Double getX0() {
+        return x0;
+    }
+
+    public void setX0(Double x0) {
+        this.x0 = x0;
+    }
+
+    public Double getY0() {
+        return y0;
+    }
+
+    public void setY0(Double y0) {
+        this.y0 = y0;
+    }
+
+    public Double getX1() {
+        return x1;
+    }
+
+    public void setX1(Double x1) {
+        this.x1 = x1;
+    }
+
+    public Double getY1() {
+        return y1;
+    }
+
+    public void setY1(Double y1) {
+        this.y1 = y1;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
